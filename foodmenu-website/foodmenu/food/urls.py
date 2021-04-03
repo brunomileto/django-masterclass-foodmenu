eeb2ff3001d8a70f9b2545735582f195f680f00a
Bruno Mileto <brunomileto@outlook.com>
2021-04-03 14:16:25 -0300

    All work for the section 10

--- foodmenu-website/foodmenu/food/urls.py
+++ foodmenu-website/foodmenu/food/urls.py
@@ -2,11 +2,16 @@ from . import views
 from django.urls import path
 app_name = 'food'
 urlpatterns = [
-    path('', views.index, name='index'),
+    #/food/
+    #path('', views.index, name='index'),  Path for function index view
+    path("", views.IndexClassView.as_view(), name="index"),
     path('item/', views.item, name='item'),
-    path('<int:item_id>', views.detail, name='detail'),
+     #/food/1
+    #path('<int:item_id>', views.detail, name='detail'), Path for function detail view
+    path('<int:pk>', views.FoodDetail.as_view(), name='detail'),
     # add items
-    path('add', views.create_item, name='create_item'),
+    #path('add', views.create_item, name='create_item'), Path for function create view
+    path('add', views.CreateItem.as_view(), name='create_item'),
     # edit items
     path('update/<int:id>', views.update_item, name='update_item'),
     # delete items
